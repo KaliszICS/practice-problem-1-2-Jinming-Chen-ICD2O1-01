ac45fa2268db02297c6c113309c3c5838a25c962
Jinming-Chen-ICD2O1-01 <jinming.chen@student.tdsb.on.ca>
2024-11-04 10:11:37 -0500

    Update PP1_2.py

--- PP1_2.py
+++ PP1_2.py
@@ -6,50 +6,50 @@
 '''
 
 def q1():
-
+# Assign the name "Alex" to the variable named name. Output its value.
 name = "Alex"
-print("name")
+print("name")# Output: Alex
 
 def q2():
-
+# Assign the integers 3, 5, and 2 to three different variables with different names. Output their sum (add them together).
 a = 3
 b = 5
 c = 2
 
 num= a + b + c
-print("num")
+print("num")# Output: 10
 
 def q3():
-
+# Assign the integers 21 and 4 to two different variables. Output the quotient (rounded down, no decimals).
 a = 21
 b = 4
 
 num = a // b
-print("num")
+print("num")# Output: 5
 
 def q4():
-
+# Assign the floating point numbers 6.3 and 3.0 to two different variables. Output the exact quotient (result of the division problem).
 a = 6.3
 b = 3.0
 num = a / b
-print("num")
+print("num")# Output: 2.1
 
 def q5():
-
+# Assign the value "Hello World" to a variable. Output it. Replace the value of the variable with "Goodbye World". Output it.
 word = "Hello World"
-print("word")
+print("word")# Output: Hello World
 
 word = "Goodbye World"
-print(word)
+print(word)# Output: Goodbye World
 
 def q6():
-
+# Assign the value True to a variable. Output it.
 word = True
-print("word")
+print("word")# Output: True
 
 def q7():
-
+# Assign the values ​​7 and 2 to two different variables. Output the remainder of the first variable (7) divided by the second variable (2).
 a = 7
 b = 2
 num = a % b
-print("num")
+print("num")# Output: 1
