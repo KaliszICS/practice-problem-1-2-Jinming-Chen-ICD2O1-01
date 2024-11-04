37359844e9daaaec504f19076efe68ca4c3a6d11
Jinming-Chen-ICD2O1-01 <jinming.chen@student.tdsb.on.ca>
2024-11-04 09:42:45 -0500

    Update PP1_2.py

--- PP1_2.py
+++ PP1_2.py
@@ -1,25 +1,53 @@
-
-
-#Write Assignment code here
+'''
+    Lesson: Variables and Data Types
+    Author:Jinming Chen
+    Date Created: Nove 16, 2024
+    Date Last Modified: Nove 16, 2024
+'''
 
 def q1():
 
+name = "Alex"
+print(name)
+
 def q2():
+
+a = 3
+b = 5
+c = 2
+num= a + b + c
+print(num)
 
 def q3():
 
+a = 21
+b = 4
+num = x // y
+print(num)
+
 def q4():
+
+a = 6.3
+b = 3.0
+num = m / n
+print(num)
 
 def q5():
 
+word = "Hello World"
+print(word)
+
+word = "Goodbye World"
+print(word)
+
 def q6():
+
+word = True
+print(word)
 
 def q7():
 
-q1()
-q2()
-q3()
-q4()
-q5()
-q6()
-q7()
+a = 7
+b = 2
+num = p % q
+print(num)
