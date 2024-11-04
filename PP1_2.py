aac5ba32987b8e32ff58a0d024a4fc78bd2d1f80
Jinming-Chen-ICD2O1-01 <jinming.chen@student.tdsb.on.ca>
2024-11-04 09:51:40 -0500

    Update PP1_2.py

--- PP1_2.py
+++ PP1_2.py
@@ -5,34 +5,36 @@
     Date Last Modified: Nove 16, 2024
 '''
 
-def q1():
+def q1(name):
 
 name = "Alex"
 print(name)
 
-def q2():
+def q2(name):
 
 a = 3
 b = 5
 c = 2
+
 num= a + b + c
 print(num)
 
-def q3():
+def q3(name):
 
 a = 21
 b = 4
-num = x // y
+
+num = a // b
 print(num)
 
-def q4():
+def q4(name):
 
 a = 6.3
 b = 3.0
-num = m / n
+num = a / b
 print(num)
 
-def q5():
+def q5(name):
 
 word = "Hello World"
 print(word)
@@ -40,14 +42,14 @@ print(word)
 word = "Goodbye World"
 print(word)
 
-def q6():
+def q6(name):
 
 word = True
 print(word)
 
-def q7():
+def q7(name):
 
 a = 7
 b = 2
-num = p % q
+num = a % b
 print(num)
